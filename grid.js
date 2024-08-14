581b10a829f45d5b2159b0dc17df73ebb5037341
suru-ach <srujanacharya36@gmail.com>
2024-08-14 19:54:49 +0530

    add populate pixel on drag

--- grid.js
+++ grid.js
@@ -1,3 +1,6 @@
+const traverseButton = document.getElementById('traverse-button');
+const clearButton = document.getElementById('clear-button');
+
 const canvas = document.getElementById('graphCanvas');
 const ctx = canvas.getContext('2d');
 const points = [];
@@ -39,7 +42,16 @@ function getMouseCoords (click_event) {
     }
 }
 
-canvas.addEventListener('click', function (click_event) {
+let clicked = 0;
+
+canvas.addEventListener('mouseup', function () {
+    clicked^=1;
+});
+
+canvas.addEventListener('mousedown', function (click_event) {
+    clicked^=1;
+    positions.clear();
+
     const {x, y} = getMouseCoords(click_event);
 
     const value = get_grid(x, y);
@@ -51,6 +63,32 @@ canvas.addEventListener('click', function (click_event) {
         ctx.fillRect(x * gridSize + 1, y * gridSize + 1, gridSize-2, gridSize-2);
     }
     set_grid(x, y, value^1);
+});
+
+let positions = new Set();
+
+canvas.addEventListener('mousemove', function (click_event) {
+    const {x, y} = getMouseCoords(click_event);
+
+    // save the values in set not to update it on every event trigger
+    let set_pos = x * canvas.width + y;
+    if(!clicked || positions.has(set_pos)) return;
+    positions.add(set_pos);
+
+    const value = get_grid(x, y);
+
+    if(value) {
+        ctx.clearRect(x * gridSize + 1, y * gridSize + 1, gridSize-2, gridSize-2);
+    } else {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(x * gridSize + 1, y * gridSize + 1, gridSize-2, gridSize-2);
+    }
+    set_grid(x, y, value^1);
+});
+
+clearButton.addEventListener('click', function() {
+    grid.fill(0); 
+    drawGraph(); 
 });
 
 drawGraph(); 
